62a20297aafa31798f49b888181293535d1ed234
Artem Morozov <deadtems@yandex.ru>
2022-09-24 07:15:12 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import ReactDOM from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom";
+
 import "./index.scss";
 import "macro-css";
+
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/">
+    <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
