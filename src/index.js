9db75cd12c66d287fdc65eefa20eaa3cd6085d50
Artem Morozov <deadtems@yandex.ru>
2022-09-24 08:43:06 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.scss";
 import "macro-css";
-
 import App from "./App";
 
 ReactDOM.render(
