fee41bf2ef6e2dd590aa99af2c0ec912d6da699d
Artem Morozov <deadtems@yandex.ru>
2022-09-24 07:00:58 +0500

    basename

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import 'macro-css';
-import App from './App';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import "macro-css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Router>
-      <App />
-    </Router>
+  <Router basename="/">
+    <App />
+  </Router>
 );
